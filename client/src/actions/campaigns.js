cf8c7e1d8fc2a70e448c1116981516d6fd65e1c9
Lukeghenco <lukeghenco@gmail.com>
2017-09-02 21:04:36 -0400

    Remove APIRequestStatus code

--- client/src/actions/campaigns.js
+++ client/src/actions/campaigns.js
@@ -1,11 +1,3 @@
-import { 
-    makingAPIRequest, 
-    successfulAPIRequest, 
-    unsuccessfulAPIRequest 
-} from './apiRequestStatus';
-
-const API_URL = 'http://localhost:3001/api';
-
 // @ Action Creators 
 export const setCampaigns = campaigns => {
     return {
@@ -36,36 +28,21 @@ export const removeCampaign = campaignId => {
 };
 
 // @ Async Actions
+const API_URL = 'http://localhost:3001/api';
+
 export const fetchCampaigns = () => {
     return dispatch => {
-        dispatch(makingAPIRequest());
         return fetch(`${API_URL}/campaigns`)
             .then(response => response.json())
             .then(campaigns => {
-                dispatch(successfulAPIRequest());
                 dispatch(setCampaigns(campaigns));
             })
-            .catch(err => dispatch(unsuccessfulAPIRequest()));
+            .catch(err => console.log(err));
     };
 };
-
-export const fetchCampaign = (campaignId) => {
-    return dispatch => {
-        dispatch(makingAPIRequest());
-        return fetch(`${API_URL}/campaigns/${campaignId}`)
-            .then(response => response.json())
-            .then(campaign => {
-                dispatch(successfulAPIRequest());
-                dispatch(setCampaigns([campaign]));
-            })
-            .catch(err => dispatch(unsuccessfulAPIRequest()));
-    };
-};
-
 
 export const createCampaign = (campaign, routerHistory) => {
     return dispatch => {
-        dispatch(makingAPIRequest());
         return fetch(`${API_URL}/campaigns`, {
             method: 'POST', 
             headers: {
@@ -77,17 +54,15 @@ export const createCampaign = (campaign, routerHistory) => {
         })
             .then(response => response.json())
             .then(campaign => {
-                dispatch(successfulAPIRequest());
                 dispatch(addCampaign(campaign));
                 routerHistory.replace('/');
             })
-            .catch(err => dispatch(unsuccessfulAPIRequest()));
+            .catch(err => console.log(err));
     };
 };
 
 export const updateCampaign = (campaign, routerHistory) => {
     return dispatch => {
-        dispatch(makingAPIRequest());
         return fetch(`${API_URL}/campaigns/${campaign.id}`, {
             method: 'PUT',
             headers: { 
@@ -103,23 +78,21 @@ export const updateCampaign = (campaign, routerHistory) => {
             dispatch(replaceCampaign(campaign));
             routerHistory.replace(`/campaigns/${campaign.id}`);
         })
-        .catch(err => dispatch(unsuccessfulAPIRequest()));
+        .catch(err => console.log(err));
     };
 };
 
 export const deleteCampaign = (campaignId, routerHistory) => {
     return dispatch => {
-        dispatch(makingAPIRequest());
         return fetch(`${API_URL}/campaigns/${campaignId}`, {
             method: 'DELETE'
         })
         .then(response => {
             if (response.ok) {
-                dispatch(successfulAPIRequest());
                 dispatch(removeCampaign(campaignId));
                 routerHistory.replace(`/campaigns`);
             } else {
-                dispatch(unsuccessfulAPIRequest());
+                console.log(err);
             }
         })
         .catch(err => unsuccessfulAPIRequest());
